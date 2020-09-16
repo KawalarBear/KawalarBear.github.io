2d9d3aebf049baedbdcf1d0bc38cd206f3c80e70
KawalarBear <lewispeters3108@gmail.com>
2020-09-16 18:17:24 +0100

    added the video lol

--- java.js
+++ java.js
@@ -12,7 +12,7 @@ tl.fromTo(hero,1, {height: '0%'},{height:'65%', ease: Power2.easeInOut})
 .fromTo(slider, 1.2, {x: "-100%"}, {x: "0%", ease:Power2.easeInOut}, "-=1.2")
 
 .fromTo(logo, 0.5, {opacity: 0, x: 30}, {opacity:1, x:0}, "-=0.5")
-.fromTo(headline, 0.5, {opacity: 0, x: 30}, {opacity:1, x:0}, "-=0.5");
+.fromTo(headline, 0.5, {opacity: 0, x: 30}, {opacity:1, x:0}, "-=0.5")
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
